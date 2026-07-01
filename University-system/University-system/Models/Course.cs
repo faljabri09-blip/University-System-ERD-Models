7170d3ed11e3d17003bab2f19bb9711b597531c3
Fatma <fatma@gmail.com>
2026-07-01 12:19:33 +0400

    Adding Course class with constrain

---
+++ University-system/University-system/Models/Course.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; // adding constriants to the model
+
+namespace University_system.Models
+{
+    public class Course
+    {
+        [Key] //primary key
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //auto generated
+        [Required] //not null
+        public int courseId { get; set; }
+
+        [Required] //not null
+        //[Index(IsUnique = true)] //unique constraint
+        [MaxLength(10)] //max length
+        public string courseCode { get; set; }
+
+        [Required] //not null
+        [MaxLength(150)] //max length
+        public string courseTitle { get; set; }
+
+        [Required]
+        [Range(1, 6)] //range constraint
+        public int creditHours { get; set; }
+
+        [ForeignKey("Department")] //foreign key
+        [Required] //not null
+        public int departmentId { get; set; } //foreign key
+
+        [ForeignKey("Instructor")] //foreign key
+        public int ? instructorId { get; set; } //foreign key // nullable a course may be unassigned 
+
+
+        [Required] //not null
+        [MaxLength(20)] //max length
+        public string semesterOffered { get; set; } 
+    }
+}
--- University-system/University-system/obj/Debug/net8.0/University-system.AssemblyInfo.cs
+++ University-system/University-system/obj/Debug/net8.0/University-system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("University-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e408b54b38e265c3a6892b6a1739ebad6b0f67d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d013e8e736dd5f8015ca5801b2b3d1b3fb2abcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("University-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("University-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
