e408b54b38e265c3a6892b6a1739ebad6b0f67d9
Fatma <fatma@gmail.com>
2026-07-01 10:59:04 +0400

    Adding student class with constrain

---
+++ University-system/University-system/Models/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace University_system.Models
+{
+
+    public class Student
+    {
+        [Key]
+        public int stusentId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string fullname { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        //[Index(IsUnique = true)]
+        public string email { get; set; }
+
+        [MaxLength(20)]
+        public string phoneNumber { get; set; }
+
+        [Required]
+        public DateTime dateOfBirth { get; set; }
+
+        [Required]
+        [Range(2000 , 2023)]
+        public int enrollmentYear { get; set; }
+
+
+        [Range(0.0, 4.0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)] // default value
+        public decimal gpa { get; set; } 
+    }
+}
---
+++ University-system/University-system/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace University_system
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
